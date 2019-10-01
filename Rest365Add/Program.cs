d9ebafdc1775b26221da729738d7702cba314f67
JD Davenport <jddavenport46@gmail.com>
2019-10-01 12:54:53 -0600

    Requirement 1 - Limit to two numbers and comma added - not cleaned up code sorry :)

--- Rest365Add/Program.cs
+++ Rest365Add/Program.cs
@@ -10,6 +10,7 @@ namespace Rest365Add
     {
         static void Main(string[] args)
         {
+            
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello Welcome to a simple adding console app, please enter your numbers you would like to add");
@@ -19,10 +20,17 @@ namespace Rest365Add
             //dList.Add('/n'.ToCharArray)
             //dList.ToArray();
             string[] values = numbers.Split(dList.ToArray());
-            double total=0;
-            bool isPositive = true;
+            if (values.Length < 3)
+            {
+                runAdd(values);
+            }
+            else
+            {
+                Console.WriteLine("This iteration limits the max numbers to two sorry");
+
+            }
             //bool isFinished = true;
-            bool isFirst = true;
+            //bool isFirst = true;
             //foreach(string st in values)
             //{
             //    var isNumeric = double.TryParse(st, out double n);
@@ -33,11 +41,12 @@ namespace Rest365Add
             //        isFirst = false;
             //    }
             //}
-            foreach (string st in values)
-            {
 
-                
-                    isFirst = false;
+            double runAdd(string[] input)
+            {
+                double total = 0;
+                foreach (string st in input)
+                {
                     if (double.Parse(st) < 0)
                     {
                         isPositive = false;
@@ -46,26 +55,21 @@ namespace Rest365Add
                     }
                     else
                     {
-                            if (double.Parse(st)>1000)
-                            { }
-                            else
-                            {
-                                total += double.Parse(st);
+                        if (double.Parse(st) > 1000)
+                        { }
+                        else
+                        {
+                            total += double.Parse(st);
 
-                            }
+                        }
                     }
-                
-                
 
 
+
+
+                }
+                return total;
             }
-            if(isPositive)
-            {
-                 Console.WriteLine("Total is: "+total);
-
-            }
-            
-
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
