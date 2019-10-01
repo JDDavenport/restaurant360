13e4f2d9117c10dcb3b2f749029a5ae118269cd9
JD Davenport <jddavenport46@gmail.com>
2019-10-01 13:05:25 -0600

    Requirement 3 - Support /n as an alternative delimiter NOTE: Bug in code the past two commits will not run without this commit

--- Rest365Add/Program.cs
+++ Rest365Add/Program.cs
@@ -14,13 +14,14 @@ namespace Rest365Add
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello Welcome to a simple adding console app, please enter your numbers you would like to add");
-            string numbers = Console.ReadLine().ToString();
-            List<char> dList = new List<char>();
-            dList.Add(',');
-            //dList.Add('/n'.ToCharArray)
+            String numbers = Console.ReadLine().ToString();
+            List<String> dList = new List<String>();
+            dList.Add(",");
+            dList.Add("/n");
             //dList.ToArray();
-            string[] values = numbers.Split(dList.ToArray());
-            runAdd(values);
+            String[] values = numbers.Split(dList.ToArray(), StringSplitOptions.None);
+            double totalAdded=runAdd(values);
+      
            
             //bool isFinished = true;
             //bool isFirst = true;
@@ -42,7 +43,7 @@ namespace Rest365Add
                 {
                     if (double.Parse(st) < 0)
                     {
-                        isPositive = false;
+                        
                         Console.WriteLine("Sorry you entered a negative number please enter only positive numbers please");
 
                     }
@@ -63,6 +64,8 @@ namespace Rest365Add
                 }
                 return total;
             }
+            Console.WriteLine("Your total is: "+ totalAdded);
+
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
